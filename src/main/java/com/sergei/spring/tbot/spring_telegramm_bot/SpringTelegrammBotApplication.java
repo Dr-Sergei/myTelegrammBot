573815cac435ea1254b475b6724174d32c97fb16
Dr-Sergei <wsergei85@yandex.ru>
2024-05-25 09:37:54 +0200

    phase 0.5: set-up

---
+++ src/main/java/com/sergei/spring/tbot/spring_telegramm_bot/SpringTelegrammBotApplication.java
@@ -0,0 +1,13 @@
+package com.sergei.spring.tbot.spring_telegramm_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTelegrammBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTelegrammBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergei/spring/tbot/spring_telegramm_bot/SpringTelegrammBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergei.spring.tbot.spring_telegramm_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTelegrammBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

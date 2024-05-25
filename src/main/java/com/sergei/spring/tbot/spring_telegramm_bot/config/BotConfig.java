a764d59d3aad31c381fd4f00fa80607eeb425213
Dr-Sergei <wsergei85@yandex.ru>
2024-05-25 14:05:55 +0200

    phase 1: first test setup done, everything works well

---
+++ src/main/java/com/sergei/spring/tbot/spring_telegramm_bot/config/BotConfig.java
@@ -0,0 +1,40 @@
+package com.sergei.spring.tbot.spring_telegramm_bot.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("application.properties")
+public class BotConfig {
+
+    @Value("${bot.name}")
+    String botName;
+
+    @Value("${bot.token}")
+    String token;
+
+    public BotConfig() {
+    }
+
+    public BotConfig(String botName, String token) {
+        this.botName = botName;
+        this.token = token;
+    }
+
+    public String getBotName() {
+        return botName;
+    }
+
+    public void setBotName(String botName) {
+        this.botName = botName;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
---
+++ src/main/java/com/sergei/spring/tbot/spring_telegramm_bot/service/TelegramBot.java
@@ -0,0 +1,65 @@
+package com.sergei.spring.tbot.spring_telegramm_bot.service;
+
+
+import com.sergei.spring.tbot.spring_telegramm_bot.config.BotConfig;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    final BotConfig botConfig;
+
+    public TelegramBot(BotConfig botConfig) {
+        this.botConfig = botConfig;
+    }
+
+
+    @Override
+    public String getBotUsername() {
+        return botConfig.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return botConfig.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+
+            long chatId = update.getMessage().getChatId();
+
+            switch (messageText) {
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                default:
+                    sendMessage(chatId, "sorry command not recognized!");
+            }
+        }
+    }
+
+    private void startCommandReceived(long chatId, String firstName) {
+        String answer = "Hi, " + firstName + ", nice to meet you!";
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}

22bd7d5294c343d2bed9661f9e88f20b3bc8b5be
Dr-Sergei <wsergei85@yandex.ru>
2024-05-26 20:17:59 +0200

    phase 5 : help information added

--- src/main/java/com/sergei/spring/tbot/spring_telegramm_bot/service/TelegramBot.java
+++ src/main/java/com/sergei/spring/tbot/spring_telegramm_bot/service/TelegramBot.java
@@ -9,7 +9,6 @@ import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
-import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScope;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -23,18 +22,24 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     private BotConfig botConfig;
 
+    final String HELP_TEXT = "if you have any problems please contact bot administrator\n\n" +
+            "You can execute commands from the main menu on the left side\n\n" +
+            "Type /start to see a welcome message\n\n" +
+            "Type /madata to see data stored about yourself\n\n" +
+            "Type /help to see this message again";
+
     public TelegramBot(BotConfig botConfig) {
         this.botConfig = botConfig;
         //menu
         List<BotCommand> listOfCommands = new ArrayList<>();
-        listOfCommands.add(new BotCommand("/start","get a welcome message"));
-        listOfCommands.add(new BotCommand("/mydata","show a users data"));
-        listOfCommands.add(new BotCommand("/deletedata","delete user data"));
-        listOfCommands.add(new BotCommand("/help","info how to use this bot"));
-        listOfCommands.add(new BotCommand("/settings","set your preferences"));
-        listOfCommands.add(new BotCommand("/createplayer","create a new player"));
+        listOfCommands.add(new BotCommand("/start", "get a welcome message"));
+        listOfCommands.add(new BotCommand("/mydata", "show a users data"));
+        listOfCommands.add(new BotCommand("/deletedata", "delete user data"));
+        listOfCommands.add(new BotCommand("/help", "info how to use this bot"));
+        listOfCommands.add(new BotCommand("/settings", "set your preferences"));
+        listOfCommands.add(new BotCommand("/createplayer", "create a new player"));
         try {
-            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(),null));
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
         } catch (TelegramApiException e) {
             log.error("Error setting bot´s command list: {}", e.getMessage());
         }
@@ -62,7 +67,10 @@ public class TelegramBot extends TelegramLongPollingBot {
 
             switch (messageText) {
                 case "/start":
-                    gereetingMessage(chatId, update.getMessage().getChat().getFirstName());
+                    greetingMessage(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                case "/help":
+                    helpInfo(chatId, update.getMessage().getChat().getFirstName());
                     break;
                 default:
                     sendMessage(chatId, "sorry command not recognized!");
@@ -70,11 +78,18 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
     }
 
+    private void helpInfo(long chatId, String firstName) {
+        String info = HELP_TEXT;
+        log.info("\n help info replied to user {}", firstName + "\n");
+
+        sendMessage(chatId, info);
+    }
+
     //taking the firs name and chat id, generating answer and sending it back
-    private void gereetingMessage(long chatId, String firstName) {
+    private void greetingMessage(long chatId, String firstName) {
         String answer = "Hi, " + firstName + ", nice to meet you!";
 
-        log.info("\n Replied to user {}", firstName +"\n");
+        log.info("\n Replied to user {}", firstName + "\n");
         sendMessage(chatId, answer);
     }
 

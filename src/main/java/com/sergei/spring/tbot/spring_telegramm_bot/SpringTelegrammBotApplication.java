0da4224cacaa86141aa64b5cc94a71d9d948b533
Wittchen <swittche@ipb-halle.de>
2024-05-30 09:58:12 +0200

    phase 6: mongo connection provided

--- src/main/java/com/sergei/spring/tbot/spring_telegramm_bot/SpringTelegrammBotApplication.java
+++ src/main/java/com/sergei/spring/tbot/spring_telegramm_bot/SpringTelegrammBotApplication.java
@@ -1,13 +1,25 @@
 package com.sergei.spring.tbot.spring_telegramm_bot;
 
+import com.sergei.spring.tbot.spring_telegramm_bot.repositroy.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-public class SpringTelegrammBotApplication {
+@EnableMongoRepositories
+public class SpringTelegrammBotApplication implements CommandLineRunner {
+
+	@Autowired
+	PlayerRepository playerRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTelegrammBotApplication.class, args);
 	}
 
+	@Override
+	public void run(final String... args) throws Exception {
+
+	}
 }
